e51aa594ca3756fb4a74c651ff2a9ab4587fa98a
Luiz Eduardo da Silva Araújo <luizsilvaa1114@gmail.com>
2021-12-28 00:54:11 -0300

    adicionando pasta aula-4

---
+++ aula-4 estruturas condicionais/assets/js/script.js
@@ -0,0 +1,85 @@
+//estrutura condicionais
+
+var jogador1 = 0;
+var jogador2 = 0;
+var placar;
+
+/*// if ternário
+jogador1 != -1  && jogador2 != -1 ? console.log('os jogadores são validos') : console.log('jogadores Inválidos')
+
+// usando if
+if (jogador1 > 0 && jogador2 == 0) {
+    console.log('jogador 1 marcou ponto!');
+    placar = jogador1 > jogador2;
+}
+
+// usando else if  
+else if(jogador2 > 0 && jogador2 == 0) {
+    console.log('jogador 2 marcou ponto!')
+    placar = jogador2 > jogador1;
+    }
+
+// usando else
+else{
+    console.log('ninguem marcou ponto!');
+}
+
+// usando switch case
+switch (placar) {
+    case placar = jogador1 > jogador2:
+    console.log('jogador 1 ganhou');
+    break;
+
+    case placar = jogador2 > jogador1:
+    console.log('jogador 2 ganhou');
+    break;
+    default :
+    console.log('ninguem ganhou')
+}
+
+//estrutura de repetição
+
+
+let array = ['valor1', 'valor2','valor3','valor4','valor5',];
+
+let Object = {propriedade: 'valor1', propriedade2: 'valor2', propriedade3:'valor3',}
+
+
+// for executa uma  intruçaõ até que ela seja falsa
+for (let indice = 0; indice < array.length; indice++) {
+    console.log(indice)
+}
+
+// for in executa repetição  a partir de uma propriedade 
+for (let i in array) {
+    console.log(i);
+}
+
+// com objeto 
+for (let i in Object) {
+    console.log(i);
+}
+
+// for of executa repetição  apartir de um valor
+for (i of array) {
+    console. log(i);
+}
+
+// com objeto não funciona pois não é iterável
+for(i of Object.propriedade) {
+console.log(i)
+}
+
+//while executa enquanto for verdadeira
+var a = 0;
+
+while(a < 10){
+    a++;
+    console.log(a);
+}
+
+// do  executa até que seja falsa
+do {
+    a++;
+    console.log(a);
+} while (a <10);*/
