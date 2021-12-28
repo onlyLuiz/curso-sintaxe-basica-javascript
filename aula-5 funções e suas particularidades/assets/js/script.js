a8347f2a9177de847ece75016a3949afa6fb2d5c
Luiz Eduardo da Silva Araújo <luizsilvaa1114@gmail.com>
2021-12-28 00:55:08 -0300

    adicionando pasta aula-5

---
+++ aula-5 funções e suas particularidades/assets/js/script.js
@@ -0,0 +1,16 @@
+// funções - functions
+
+// como declarar: deve ser declarada usando a palavra function seguida de "()" e "{}"
+/*function funcao() {
+    console.log('Essa mensagem veio de uma função!');
+}
+//chamando função
+funcao();*/
+
+// fuction com parametro
+// definindo parâmetros
+function mensagem(primeiro, segundo) {
+    console.log(primeiro, segundo);
+}
+// chamando a função atribuindo valor aos parâmetros
+mensagem('olá', 'como você está?');
