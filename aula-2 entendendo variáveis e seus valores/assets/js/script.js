f4dd1fb1ce85356e9ab6c3f179838f3dcbea9a16
Luiz Eduardo da Silva Araújo <luizsilvaa1114@gmail.com>
2021-12-28 00:52:14 -0300

    adicionando pasta aula-2

---
+++ aula-2 entendendo variáveis e seus valores/assets/js/script.js
@@ -0,0 +1,104 @@
+/*   // tipos premitivos 
+
+   //boolean
+   var boolean = false;
+   console.log(typeof(boolean)) ;
+
+   // number
+   var numero = 10;
+   console.log(typeof(numero));
+
+   //string
+   var nome = 'Luiz';
+   console.log(typeof(nome));
+
+   // declaração de variável
+   var; // variável global ou local - valor inicial nulo
+   let; // variavel local de bloco - valor inicial nulo
+   const; // variável local de bloco - valor inicial obrigatório */
+
+   /*var variavel = 'luiz'
+   var variavel = 'eduardo'
+   console.log(variavel);
+
+   let variavel2 = 'luiz'
+   variavel2 = 'eduardo'
+   console.log(variavel2);
+
+   const constante = 10;
+   console.log(constante);
+   
+   // variável global
+   var escopoGlobal = 'global';
+   console.log(escopoGlobal);
+
+   //variável local
+  function escopoLocal() {
+      let escopoLocalInterno = 'local';
+      console.log(escopoLocalInterno);
+  }
+  escopoLocal();
+
+   //atribuição
+   var atribuicao = 'luiz';
+   console.log(atribuicao);
+
+   //comparacao
+   var comparacao  = '0' == 0;
+   console.log(comparacao); 
+
+   //comparacao identica
+   var comparacaoIdentica = '0' === 0;
+   console.log(comparacaoIdentica); 
+
+   //operadores aritméticos
+
+   // adicao
+   var adicao = 10 + 2;
+   console.log(adicao);
+
+   // subtracao
+   var subtracao = 50 - 20;
+   console.log(subtracao);
+
+   // multiplicacao
+   var multiplicacao = 5 * 2;
+   console.log(multiplicacao);
+
+   // divisao real
+   var divisaoReal = 30 / 2;
+   console.log(divisaoReal);
+
+   // potenciacao
+   var potenciacao = 30 ** 5;
+   console.log(potenciacao); 
+
+   // maior que
+   var maiorQue = 5 > 2;
+   console.log(maiorQue);
+
+   // menor que
+   var menorQue = 5 < 2;
+   console.log(menorQue);
+
+   // maior ou igual a
+   var maiorOuIgual = 5 >= 2;
+   console.log(maiorOuIgual);
+   
+   // menor ou igual 
+   var menorOuIgual = 5 <= 2;
+   console.log(menorOuIgual); 
+
+   // operadores lógicos
+
+   // && considera que todos os valores sejam true
+   var e = true &&  false;
+   console.log(e);
+
+   // || consira que qualquer valor seja verdadeiro
+   var ou = true || false;
+   console.log(ou);
+
+   // ! inverte o valor de true para false ou vice-versa
+   var nao = ! true;
+   console.log(nao); */
